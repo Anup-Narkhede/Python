db39742e2f0f9351f9a789b818601ae2331b38ec
Anup Anant Narkhede <72505372+Anup-Narkhede@users.noreply.github.com>
2021-10-05 17:42:28 +0530

    Update 1.Oddeven.py

--- 1.Oddeven.py
+++ 1.Oddeven.py
@@ -3,7 +3,7 @@
 
 n=int(input("Enter a natural number to check whether it is Even or Odd:  "))
 
-if (n % 2 == 0):                        # Modulus Operator:  gives remainder e.b. 12 % 4 =  0
+if (n % 2 == 0):                        # Modulus Operator:  gives remainder e.g. 12 % 4 =  0
     print(n," is an Even Number")
 else:
     print(n," is an Odd Number")
